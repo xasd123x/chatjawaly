4e680b0b6576542cf4976f5a18609d9dcd2b02e5
xasd123x <153305949+xasd123x@users.noreply.github.com>
2024-04-16 01:52:49 +0300

    Update and rename room-alhla6.js to room-alhla7.js

--- room-alhla7.js
+++ room-alhla7.js
@@ -34,7 +34,7 @@ margin-left: -200px !important;
   margin-top: 40px !important;
   margin-bottom: 0px !important;
   background-color: #d9534f00!important;
-  margin-left: -10px !important;
+  margin-left: -20px !important;
 }
 
 .nosel.d-flex.room.r18ee3d707d9xaf87ad06-xj8dpir.bord {border-inline: 0px solid #4c121e !important;}
